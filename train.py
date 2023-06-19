7855d35d6278f9e45b6b0c774a5ca281d0cb768a
Alonso Villegas <alonsovillegas816@gmail.com>
2023-06-19 07:58:53 -0500

    fix merge conflicts

--- train.py
+++ train.py
@@ -4,14 +4,28 @@ from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import StackingClassifier
 from sklearn.svm import SVC
 import xgboost as xgb
 
 
 def roc_auc_score(model, x, y):
     return metrics.roc_auc_score(y, model.predict_proba(x)[:, 1])
+
+
+def train_stacking_model(estimators, x_train, y_train, x_test, y_test):
+    stacking_model = StackingClassifier(estimators=estimators,
+                                        final_estimator=LogisticRegression())
+    stacking_model.fit(x_train, y_train)
+
+    train_score = roc_auc_score(stacking_model, x_train, y_train)
+    test_score = roc_auc_score(stacking_model, x_test, y_test)
+
+    return train_score, test_score, stacking_model
 
 
 def train_model(name, x_train, y_train, x_test, y_test):
@@ -24,7 +38,7 @@ def train_model(name, x_train, y_train, x_test, y_test):
         model = RandomForestClassifier(random_state=33)
     elif name == "SVM":
         parameters = {
-            "C": [0.1, 1, 10, 100, 500],
+            "C": [0.1, 1, 10, 100],
             "kernel": ["linear", "rbf", "sigmoid"],
             "gamma": ["scale", "auto"]
         }
@@ -35,12 +49,18 @@ def train_model(name, x_train, y_train, x_test, y_test):
             "n_estimators": [100, 500, 1000, 2000]
         }
         model = xgb.XGBClassifier()
-     
-    elif name == "LogisticRegression":
+    elif name == "Logistic Regression":
         parameters = {
-            "C":[0.01, 0.1, 1, 10, 100, 500]
+            "C": [0.01, 0.1, 1, 10, 100, 500]
         }
-        model = LogisticRegression(random_state=0,C=1.0)    
+        model = LogisticRegression(random_state=0, C=1.0)
+    elif name == "MLP":
+        parameters = {
+            "hidden_layer_sizes": [[100], [200], [300], [500]],
+            "activation": ["identity", "logistic", "tanh", "relu"],
+            "max_iter": [500, 700]
+        }
+        model = MLPClassifier(random_state=33)
     else:
         raise NotImplementedError()
 
@@ -51,23 +71,39 @@ def train_model(name, x_train, y_train, x_test, y_test):
 
     if name == "Random Forest":
         model_tuned = RandomForestClassifier(
-            n_estimators=best_params["n_estimators"], max_depth=best_params["max_depth"], max_features=best_params["max_features"], random_state=33)
+            n_estimators=best_params["n_estimators"],
+            max_depth=best_params["max_depth"],
+            max_features=best_params["max_features"],
+            random_state=33
+        )
     elif name == "SVM":
-        model_tuned = SVC(C=best_params["C"],
-                          probability=True, random_state=33)
+        model_tuned = SVC(
+            C=best_params["C"],
+            kernel=best_params["kernel"],
+            gamma=best_params["gamma"],
+            probability=True,
+            random_state=33
+        )
     elif name == "XGBoost":
         model_tuned = xgb.XGBClassifier(
             n_estimators=best_params["n_estimators"], max_depth=best_params["max_depth"])
-    elif name == "LogisticRegression":
-        model_tuned = LogisticRegression(
-            random_state=0,C=best_C)
+    elif name == "Logistic Regression":
+        model_tuned = LogisticRegression(random_state=33, C=best_params["C"])
+    elif name == "MLP":
+        model_tuned = MLPClassifier(
+            hidden_layer_sizes=best_params["hidden_layer_sizes"],
+            activation=best_params["activation"],
+            max_iter=best_params["max_iter"]
+        )
     else:
         raise NotImplementedError()
 
     model_tuned.fit(x_train, y_train)
 
-    print("Train AUC:", roc_auc_score(model_tuned, x_train, y_train))
-    print("Test AUC:", roc_auc_score(model_tuned, x_test, y_test))
+    train_score = roc_auc_score(model_tuned, x_train, y_train)
+    test_score = roc_auc_score(model_tuned, x_test, y_test)
+
+    return train_score, test_score, model_tuned
 
 
 dataset = pd.read_csv("./in-vehicle-coupon-recommendation-processed.csv")
@@ -78,11 +114,24 @@ y = dataset["Y"]
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=44)
 
-print_title("Entrenando Modelos")
-
-
-models = ["Random Forest", "XGBoost", "SVM","LogisticRegression"]
+models = ["Random Forest", "SVM", "XGBoost", "MLP"]
+stacking_estimators = []
 
 for model in models:
     print_title(model)
-    train_model(model, x_train, y_train, x_test, y_test)
+
+    train_score, test_score, model_tuned = train_model(
+        model, x_train, y_train, x_test, y_test)
+
+    print("Train AUC:", train_score)
+    print("Test AUC:", test_score)
+
+    stacking_estimators.append((model, model_tuned))
+
+print(stacking_estimators)
+
+train_score, test_score, model_tuned = train_stacking_model(
+    stacking_estimators, x_train, y_train, x_test, y_test)
+
+print("Train AUC:", train_score)
+print("Test AUC:", test_score)
